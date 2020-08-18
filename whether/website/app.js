1ae550b203ed24505e46e411a2be28b06023cd83
omar-super-star <66751365+omar-super-star@users.noreply.github.com>
2020-08-18 21:22:26 +0200

    Update app.js

--- whether/website/app.js
+++ whether/website/app.js
@@ -9,7 +9,7 @@ const content=document.getElementById("content")
 const feelings=document.getElementById('feelings')
 // Create a new date instance dynamically with JS
 let d = new Date();
-let newDate = d.getMonth()+'.'+ d.getDate()+'.'+ d.getFullYear();
+let newDate = d.getMonth()+1+'.'+ d.getDate()+'.'+ d.getFullYear();
 
 
 
